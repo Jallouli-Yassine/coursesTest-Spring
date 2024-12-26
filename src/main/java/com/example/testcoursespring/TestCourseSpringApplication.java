41e8af518bd06e9ee3b54f5b34faa3d0d82f7f12
jallouli yassine <jungloul1@gmail.com>
2024-12-26 01:56:35 +0100

    first commit

---
+++ src/main/java/com/example/testcoursespring/TestCourseSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcoursespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestCourseSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestCourseSpringApplication.class, args);
+    }
+
+}
